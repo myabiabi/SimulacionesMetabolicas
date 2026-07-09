ed4dc10ad962fa1f1d47871a9d975d8cea0c7064
Mariana Abigail Montante Arenas <mmontante@login01.cluster>
2026-07-09 17:22:31 -0600

    scrip para comunidades

---
+++ work/f3/f28a731762889d74fbc7dedf648cb2/.command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ue
+eval "$(mamba shell hook --shell bash)"
+mamba activate python3
+module load COMETS
+
+python3 /mnt/data/sur/users/mmontante/SimulacionesMetabolicas/sim_syncom_comets.py \
+    --gem_path /mnt/data/sur/users/mmontante/02_resultados/rz/gem \
+    --strains rz_pk_gp_lb_ST00060 \
+    --initial_mass 1e-4 \
+    --cycles 5 \
+    --media lb \
+    --outdir resultado_rz_pk_gp_lb_ST00060
