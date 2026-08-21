c3b9e1350381523564fb2af9715f93e9126e0d9d
myabiabi <abigail.montante.arenas@gmail.com>
2026-08-21 13:17:05 -0600

    agregue mobd al medio

--- comets_functions.py
+++ comets_functions.py
@@ -190,6 +190,7 @@ def media(name="lb", dil=0.1, vol=0.03):
             "pydx_e": 0.000443314813 * vol * dil,
             "chol_e": 0.0153595085 * vol * dil,
             "adocbl_e": 2.58226354E-09 * vol * dil,
+            "mobd_e":     3.07E-04        * vol * dil,
             "o2_e": 18.2 * vol,
             "h2o_e": 55509.29781 * vol,
             "h_e": 1E-04 * vol
@@ -266,7 +267,6 @@ def media(name="lb", dil=0.1, vol=0.03):
         "nac_e": 0.00326 * vol * dil,
         "pydxn_e": 0.0000886 * vol * dil,
         "cbl1_e": 0.000000000516 * vol * dil,
-        "mobd_e":     3.07E-04        * vol * dil,
         "o2_e": 18.2 * vol,
         "h2o_e": 55509.2978073827 * vol,
         "h_e": 0.0001 * vol,
